704ec05a6d1ab4cb67bc2e5068c25ecc0ece474f
Daniele Margutti <hello@danielemargutti.com>
2018-10-13 13:16:12 +0200

    iOS8 Example support

--- ExampleApp/Table Example/TableViewController.swift
+++ ExampleApp/Table Example/TableViewController.swift
@@ -79,7 +79,7 @@ public class ArticleAdapter: TableAdapter<Article,TableArticleCell> {
 			ctx.cell?.subtitleLabel?.text = ctx.model.text
 		}
 		self.on.tap = { ctx in
-            ctx.cell?.accessoryType = UITableViewCellAccessoryType.checkmark
+			ctx.cell?.accessoryType = UITableViewCell.AccessoryType.checkmark
 			print("Tapped on article \(ctx.model.modelID)")
 			return .deselectAnimated
 		}
